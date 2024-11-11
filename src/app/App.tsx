a91790790e5ffe9b6913ba21dcd10c153d3e62ad
Jose Manuel Veliz <jose.manuel.veliz.p@gmail.com>
2024-11-11 10:02:50 -0400

    Start project

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/libs/shadcn/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <Button>
+        Hola
+      </Button>
+    </>
+  )
+}
+
+export default App
